64ccef77e2d94250e04e26fad45ba64ff40030b5
Galitsky Dmytro <melles.android@gmail.com>
2024-03-21 23:04:26 +0200

    Add animated text printing effect and improve toolbar title handling
    Description:
    Implemented a new feature to add an animated text printing effect to the setToolbarText method in the ToolbarTitleUtils class. This effect gradually reveals the text, simulating typing, and includes a blinking underscore character for added visual interest. The implementation ensures smooth and intuitive title updates for better user experience.
    
    Additionally, improved the handling of toolbar titles by applying an alpha animation to the title text, enhancing its visibility and aesthetic appeal. This animation smoothly fades in the title text when updated, providing a more polished appearance.
    
    Changes Made:
    - Added animated text printing effect to setToolbarText method
    - Improved toolbar title handling with alpha animation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("androidx.compose.ui:ui-unit-android:1.6.3")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("androidx.core:core-animation:1.0.0-rc01")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/craftrom/manager/core/utils/ToolbarTitleUtils.kt
+++ app/src/main/java/com/craftrom/manager/core/utils/ToolbarTitleUtils.kt
@@ -1,5 +1,9 @@
 package com.craftrom.manager.core.utils
 
+
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AlphaAnimation
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.craftrom.manager.R
@@ -10,10 +14,46 @@ object ToolbarTitleUtils {
             val titleTextView = activity.findViewById<TextView>(R.id.toolbar_title)
             val subtitleTextView = activity.findViewById<TextView>(R.id.toolbar_subtitle)
 
+            // Змінюємо прозорість заголовка
+            animateTextView(titleTextView)
             titleTextView.text = title
-            subtitleTextView.text = subtitle
             titleTextView.maxLines = 1
+
+            // Застосовуємо ефект друкування до підзаголовка
+            animateTextPrinting(subtitleTextView, subtitle ?: "")
             subtitleTextView.maxLines = 1
         }
     }
+
+    private fun animateTextPrinting(textView: TextView, text: String) {
+        val textLength = text.length
+        val delayMillis: Long = 100 // Затримка між відображенням кожного символу
+
+        val handler = Handler(Looper.getMainLooper())
+        var currentLength = 0
+
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                if (currentLength <= textLength) {
+                    val displayedText = if (currentLength % 2 == 0 && currentLength < textLength) {
+                        text.substring(0, currentLength) + "_"
+                    } else {
+                        text.substring(0, currentLength)
+                    }
+                    textView.text = displayedText
+                    currentLength++
+                    handler.postDelayed(this, delayMillis)
+                }
+            }
+        }, delayMillis)
+    }
+
+    private fun animateTextView(textView: TextView) {
+        val anim = AlphaAnimation(0.0f, 1.0f)
+        anim.duration = 300 // Тривалість анімації у мілісекундах
+        textView.startAnimation(anim)
+    }
 }
+
+
+
